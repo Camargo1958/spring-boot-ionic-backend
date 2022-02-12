8490824dba3b3d467ef67c5064f4822d37120629
Camargo1958 <aldrov_camargo@hotmail.com>
2022-02-12 17:34:07 -0300

    Criando operacao de instanciacao

--- src/main/java/com/hazak/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/hazak/cursomc/resources/CategoriaResource.java
@@ -23,12 +23,12 @@ public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat3 = new Categoria(3, "Informática2");
+		Categoria cat4 = new Categoria(4, "Escritório2");
 		
 		List <Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
+		lista.add(cat3);
+		lista.add(cat4);
 		
 		return lista;
 	}
