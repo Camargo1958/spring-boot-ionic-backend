f21f1bd52ec5e08ecff0cb3f43e5d8c8bc0ce80d
Camargo1958 <aldrov_camargo@hotmail.com>
2022-02-13 23:09:09 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/hazak/cursomc/services/CategoriaService.java
+++ src/main/java/com/hazak/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.hazak.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hazak.cursomc.domain.Categoria;
 import com.hazak.cursomc.repositories.CategoriaRepository;
+import com.hazak.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +18,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
