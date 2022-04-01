999a64809d48cbd5de6b84f3643547921db6e045
Camargo1958 <aldrov_camargo@hotmail.com>
2022-03-31 21:28:48 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/hazak/cursomc/domain/Pagamento.java
+++ src/main/java/com/hazak/cursomc/domain/Pagamento.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hazak.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -24,6 +25,8 @@ public abstract class Pagamento implements Serializable{
 	
 	private Integer estado;
 	
+	//@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
