da62b69563b2d268096de150afcad246dab3a522
Camargo1958 <aldrov_camargo@hotmail.com>
2022-03-31 21:50:34 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/hazak/cursomc/domain/Cliente.java
+++ src/main/java/com/hazak/cursomc/domain/Cliente.java
@@ -15,9 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hazak.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -33,7 +31,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	//@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
